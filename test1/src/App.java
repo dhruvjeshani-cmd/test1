27a7be0f2b456ab8a7eccf7986791a0216058bdd
dhruvjeshani-cmd <dhruv.jeshani@strathmore.edu>
2026-01-19 13:39:01 +0400

    Hello

--- test1/src/App.java
+++ test1/src/App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, DJ!");
+
+        String name = "Sean";
+        int a, b, c ;
+        double price1 = 123.147;
+        // float price2 = 123.147;
+
+        
     }
 }
